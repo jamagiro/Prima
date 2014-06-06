82e8aefca643479f392e6e31a71990597d919e65
Davis Rixi <dari3ndoomed@gmail.com>
2014-06-06 12:01:17 -0500

    Implementa clase Prima

--- src/prima/Prima.java
+++ src/prima/Prima.java
@@ -17,6 +17,22 @@ public class Prima {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Cliente cliente = new Cliente("Janina", "Giraldo", 47834622);
+        
+        Privado privado = new Privado("lujo", "BMW", "X6", 2012, 80000);
+        Publico publico = new Publico("ABC123", 30, "Modasa", "M5", 2010, 60000);
+        
+        cliente.agregarVehiculos(privado);
+        cliente.agregarVehiculos(publico);
+        
+        
+        double totalApagar = 0;
+        totalApagar = cliente.calcularTotalaPagar(2014);
+        
+
+        System.out.println("Total a pagar: " + totalApagar);
+        
     }
     
     
